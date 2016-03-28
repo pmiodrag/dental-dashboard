2d9362c50754211e17c7c5e22bd07ea04393f34f
miodrag <mpavkovic@gmail.com>
2016-03-28 10:07:13 +0200

    changes to merge.

--- src/client/app/components/pacients/pacients.ts
+++ src/client/app/components/pacients/pacients.ts
@@ -9,11 +9,12 @@ import { SortByDirective } from '../../shared/directives/sortby.directive';
 import { CapitalizePipe } from '../../shared/pipes/capitalize.pipe';
 import { TrimPipe } from '../../shared/pipes/trim.pipe';
 import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from "ng2-material/all";
+import { PacientFormComponent } from './pacient-form'
 @Component({ 
   selector: 'pacients', 
   providers: [DataService],
   templateUrl: 'app/components/pacients/pacients.html',
-  directives: [CORE_DIRECTIVES, RouterLink, FilterTextboxComponent, SortByDirective],
+  directives: [CORE_DIRECTIVES, RouterLink, FilterTextboxComponent, SortByDirective, PacientFormComponent],
   pipes: [CapitalizePipe, TrimPipe]
 })
 
@@ -27,7 +28,6 @@ export class PacientsComponent {
   filteredPacients: any[] = [];
   sorter: Sorter;
   pacient : Pacient;
-  pacientOBJ : Pacient;
   constructor(private dataService: DataService) { }
   
   ngOnInit() {
@@ -95,16 +95,59 @@ export class PacientsComponent {
 
 }
 
-export interface Pacient {
+// TypeScript
+interface ICar{
+     id: number; 
+    engine: string;
+    color: string;
+}
+
+class Car implements ICar {
+    constructor (public id: number, public engine: string, public color: string) {
+    }
+} 
+
+export interface IPacient {
     id: number; 
     firstName: string;
     lastName: string;
-    middleName: string,
-    gender: string,
+    middleName: string;
+    gender: string;
     address: string;
     place: string;    
     birthDate: string;
     email : string;
     phone: number;
     mobilePhone: number;
+}
+
+export class Pacient implements IPacient {
+    constructor (public id: number, public firstName: string, public lastName: string, public middleName: string,
+                public gender: string, public address: string, public place: string,  public birthDate: string, public email : string,
+                public phone: number,  public mobilePhone: number) {
+    }
+//    id: number; 
+//    firstName: string;
+//    lastName: string;
+//    middleName: string;
+//    gender: string;
+//    address: string;
+//    place: string;    
+//    birthDate: string;
+//    email : string;
+//    phone: number;
+//    mobilePhone: number;
+
+////        this.id = id; 
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.middleName = middleName,
+//        this.gender = gender,
+//        this.address = address;
+//        this.place = place;    
+//        this.birthDate = birthDate;
+//        this.email = email;
+//        this.phone = phone;
+//        this.mobilePhone = mobilePhone;
+//    }
 }
