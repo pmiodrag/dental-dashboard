3628272bcca0434aa6ab921db59b518076794011
Miroslav Pavkovic <ms.pavkovic@levi9.com>
2016-04-08 17:48:11 +0200

    Treatment service added

--- src/client/app/components/patients/patients.ts
+++ src/client/app/components/patients/patients.ts
@@ -2,7 +2,7 @@ import { Component, Input } from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import { RouterLink } from 'angular2/router';
 import { Observable } from 'rxjs/Observable';
-import { DataService } from '../../shared/services/data.service';
+import { TreatmentService } from '../../services/treatmentService';
 import { Sorter } from '../../shared/sorter';
 import { FilterTextboxComponent } from './filterTextbox.component';
 import { SortByDirective } from '../../shared/directives/sortby.directive';
---
+++ src/client/app/services/treatmentService.ts
@@ -0,0 +1,25 @@
+import { Injectable } from 'angular2/core';
+import { Http, Response, Headers, RequestOptions } from 'angular2/http';
+//Grab everything with import 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class TreatmentService {
+    baseUrl: string = '/';
+    constructor(private http: Http) { }
+    
+    
+    getTreatments(){
+        return this.http.get(this.baseUrl + '/treatments')
+                      .map((res: Response) => res.json())
+                      .catch(this.handleError);               
+    }
+    
+    handleError(error: any) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+}
