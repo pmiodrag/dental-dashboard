32a405c9ea3bc4fe57ee25700080039faa47c26e
miodrag <mpavkovic@gmail.com>
2016-06-09 15:05:49 +0200

    removing files and folders which are not used anymore.

--- src/client/app/components/rd-loading/rd-loading.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {CORE_DIRECTIVES} from '@angular/common';
-@Component({
-  selector: 'rd-loading',
-  templateUrl: 'app/components/rd-loading/rd-loading.html'
-})
-export class RdLoading {
-
-}
