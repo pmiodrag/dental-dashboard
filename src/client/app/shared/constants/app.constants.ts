ced1b2f03748495652adea5bdecfaf0b0577affe
miodrag <mpavkovic@gmail.com>
2016-06-24 11:40:16 +0200

    treatment owner constants.

--- src/client/app/shared/constants/app.constants.ts
+++ src/client/app/shared/constants/app.constants.ts
@@ -1,1 +1,5 @@
+// icon class contstant use in html.
 export var ICON_CLASS: string = "md-primary";
+//treatment owner constants.
+export var PATIENT_OWNER: string = "patient";
+export var DOCTOR_OWNER: string = "doctor";
