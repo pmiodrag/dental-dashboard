930394486f399676b04a1cc85fa8a0c82ac3bf45
miodrag <mpavkovic@gmail.com>
2016-06-27 14:15:53 +0200

    typings adjustments and upgrading angular libraries to rc-3

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../node_modules/angular2/typings/browser.d.ts" />
-
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import { bind, provide } from '@angular/core';
 import { FORM_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy, PathLocationStrategy } from "@angular/common";
